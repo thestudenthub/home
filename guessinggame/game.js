a45a20ac7a837bb4c41f373025c6616a274b2675
AnxiousPanda <39785551+AnxiousPanda@users.noreply.github.com>
2018-06-21 16:27:43 +1000

    Update game.js

--- guessinggame/game.js
+++ guessinggame/game.js
@@ -1,16 +1,16 @@
 function game() {
   let randomNumber = Math.floor(Math.random()*1000);
-  var text = "";
+  let text = "";
   document.getElementById("paragraph").innerHTML = text;
   var userGuess = prompt("Guess a number between 1 and 1000!");
   while (userGuess !== randomNumber) {
     if (parseInt(userGuess) > randomNumber) {
-      var text = var text + "\n Your last guess was: " + parseInt(userGuess);
+      let text = text + "\n Your last guess was: " + parseInt(userGuess);
       var userGuess = prompt("Guess Lower!");
     } else if (parseInt(userGuess) < randomNumber) {
-      var text = var text + "\n Your last guess was: " + parseInt(userGuess);
+      let text = text + "\n Your last guess was: " + parseInt(userGuess);
       var userGuess = prompt("Guess Higher!");
     }
-    var text = var text + "\n You guessed the number! Reload the page to try again!";
+    let text = text + "\n You guessed the number! Reload the page to try again!";
   }
 }
