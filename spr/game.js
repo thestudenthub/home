ae65e23e69d08aad88541318ef6341d96c565e06
AnxiousPanda <39785551+AnxiousPanda@users.noreply.github.com>
2018-06-25 14:30:19 +1000

    Update game.js

--- spr/game.js
+++ spr/game.js
@@ -26,33 +26,33 @@ function sprGame() {
   }
   var compare = function(choice1, choice2) {
     if (choice1 === choice2) {
-      computerOutput.innerHTML = "Computer Chose: " + computerChoice + "It's A Draw!" + "<br />";
+      computerOutput.innerHTML = "Computer Chose: " + computerChoice + "<br />" + "It's A Draw!";
       return alert ("Its a Draw!");
     }
     if (choice1 === "Rock") {
       if (choice2 === "Scissors") {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Win!" + "<br />";
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "<br />" + "You Win!";
         return alert ("You Win!");
       } else {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Lose!" + "<br />";
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "<br />" + "You Lose!";
         return alert ("You Lose!");
       }
     }
     if (choice1 === "Scissors") {
       if (choice2 === "Paper") {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Win!" + "<br />";
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "<br />" + "You Win!";
         return alert ("You Win!");
       } else {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Lose!" + "<br />";
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "<br />" + "You Lose!";
         return alert ("You Lose!");
       }
     }
     if (choice1 === "Paper") {
       if (choice2 === "Rock") {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Win!" + "<br />";
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "<br />" + "You Win!";
         return alert ("You Win!");
       } else {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Lose!" + "<br />";
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "<br />" + "You Lose!";
         return alert ("You Lose!");
       }
     }
