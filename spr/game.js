7c8de49169113cffcf37e86547e82d26e7033f8f
AnxiousPanda <39785551+AnxiousPanda@users.noreply.github.com>
2018-06-25 13:37:49 +1000

    Update game.js

--- spr/game.js
+++ spr/game.js
@@ -8,51 +8,51 @@ function sprGame() {
   } else if (userChoice !== "Rock") {
     if (userChoice !== "Paper") {
       if (userChoice !== "Scissors") {
-        computerOutput.innerHTML = "Please input either Scissors, Paper or Rock! Use Capitals!" + "\n";
+        computerOutput.innerHTML = "Please input either Scissors, Paper or Rock! Use Capitals!" + <br>;
       }
     }
   } else {
-    computerOutput.innerHTML = `Player 1: ${userChoice}` + "\n";
+    computerOutput.innerHTML = `Player 1: ${userChoice}` + <br>;
   }
   if (computerChoice < 0.34) {
     (computerChoice = "Scissors");
-    computerOutput.innerHTML = `Computer Chose: ${computerChoice}` + "\n";
+    computerOutput.innerHTML = `Computer Chose: ${computerChoice}` + <br>;
   } else if (computerChoice >= 0.67) {
     (computerChoice = "Paper");
-    computerOutput.innerHTML = `Computer Chose: ${computerChoice}` + "\n";
+    computerOutput.innerHTML = `Computer Chose: ${computerChoice}` + <br>;
   } else {
     (computerChoice = "Rock");
-    computerOutput.innerHTML = `Computer Chose: ${computerChoice}` + "\n";
+    computerOutput.innerHTML = `Computer Chose: ${computerChoice}` + <br>;
   }
   var compare = function(choice1, choice2) {
     if (choice1 === choice2) {
-      computerOutput.innerHTML = "Computer Chose: " + computerChoice + "\n" + "It's A Draw!"
+      computerOutput.innerHTML = "Computer Chose: " + computerChoice + "It's A Draw!" + <br>
       return alert ("Its a Draw!");
     }
     if (choice1 === "Rock") {
       if (choice2 === "Scissors") {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "\n" + "You Win!"
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Win!" + <br>
         return alert ("You Win!");
       } else {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "\n" + "You Lose!"
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Lose!" + <br>
         return alert ("You Lose!");
       }
     }
     if (choice1 === "Scissors") {
       if (choice2 === "Paper") {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "\n" + "You Win!"
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Win!" + <br>
         return alert ("You Win!");
       } else {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "\n" + "You Lose!"
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Lose!" + <br>
         return alert ("You Lose!");
       }
     }
     if (choice1 === "Paper") {
       if (choice2 === "Rock") {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "\n" + "You Win!"
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Win!" + <br>
         return alert ("You Win!");
       } else {
-        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "\n" + "You Lose!"
+        computerOutput.innerHTML = "Computer Chose: " + computerChoice + "You Lose!" + <br>
         return alert ("You Lose!");
       }
     }
